6bb692349a6a1f3aebc4ba092eade978401b5be5
Vishal Gautam <vineetgau@32.in>
2026-03-27 00:00:03 +0530

    Initial commit

--- models/listing.js
+++ models/listing.js
@@ -12,9 +12,7 @@ const listingSchema = new mongoose.Schema({
         url: String,
         filename: String,
 
-        // default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fillustrations%2Fdraw-nature-landscape-free-image-3583548%2F&psig=AOvVaw18IurVdeLR60KNzQPmELLT&ust=1757005674052000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMiwqauKvY8DFQAAAAAdAAAAABAX",
-        // set: (v) => v === "" ? "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fillustrations%2Fdraw-nature-landscape-free-image-3583548%2F&psig=AOvVaw18IurVdeLR60KNzQPmELLT&ust=1757005674052000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMiwqauKvY8DFQAAAAAdAAAAABAX" : v,
-
+       
     },
     price: {
         type: Number,
